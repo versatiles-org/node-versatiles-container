3f2cd264b924dd60814918952c266820d98f62c8
Michael Kreil <github@michael-kreil.de>
2023-10-19 17:19:51 +0200

    implement server

--- server.js
+++ server.js
@@ -3,182 +3,178 @@
 
 import url from 'node:url';
 import { createServer } from 'node:http';
-import { } from 'node:fs';
+import zlib from 'node:zlib';
+import { basename, resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import { Versatiles } from './index.js';
 
-// create webserver (please don't use this in production)
-export function server() {
-	const encodings = {
-		gzip: "gzip",
-		brotli: "br",
+const __dirname = (new URL('../', import.meta.url)).pathname;
+console.log({ __dirname });
+
+const ENCODINGS = {
+	gzip: 'gzip',
+	brotli: 'br',
+}
+
+const MIMETYPES = {
+	bin: 'application/octet-stream',
+	png: 'image/png',
+	jpeg: 'image/jpeg',
+	webp: 'image/webp',
+	avif: 'image/avif',
+	svg: 'image/svg+xml',
+	pbf: 'application/x-protobuf',
+	geojson: 'application/geo+json',
+	topojson: 'application/topo+json',
+	json: 'application/json',
+}
+
+export class Server {
+	opt = {
+		recompress: false,
 	};
+	layers = {};
+	server;
+	constructor(sources, opt) {
+		Object.assign(this.opt, opt)
 
-	const srvr = createServer(async (req, res) => {
+		if (!Array.isArray(sources)) sources = [sources];
 
-		res.setHeader("Content-type", "text/plain");
-		if (req.method !== "GET") return res.statusCode = 405, res.end("Method not allowed");
-		const p = url.parse(req.url).pathname;
+		sources.map(async src => {
+			let container, name;
 
-		// construct base url from request headers
-		const baseurl = this.opt.base || format("%s://%s", (req.headers["x-forwarded-proto"] || "http"), (req.headers["x-forwarded-host"] || req.headers.host));
+			if (src instanceof Versatiles) {
+				container = src;
+			} else {
+				container = new Versatiles(src);
+				name = basename(src).replace(/\..*?$/, '');
+			}
 
-		// output cache
-		const cache = {};
+			this.layers[name] = { container, name };
+		})
+	}
+	async #prepareLayers() {
+		await Promise.all(
+			Array.from(Object.values(this.layers))
+				.map(async layer => {
+					let header;
 
-		switch (p) {
-			case "/":
-			case "/index.html":
+					if (!layer.mime) {
+						header ??= await container.getHeader();
+						layer.mime = MIMETYPES[header.tile_format] || 'application/octet-stream';
+					}
 
-				// try from cache
-				if (cache.html) return res.setHeader("Content-type", "text/html; charset=utf-8"), res.end(cache.html);
+					if (!layer.precompression) {
+						header ??= await container.getHeader();
+						layer.precompression = header.precompression;
+					}
+				})
+		)
+		return this.layers;
+	}
+	async start() {
+		const layers = await this.#prepareLayers();
+		const recompress = this.opt.recompress || false;
 
-				fs.readFile(path.resolve(__dirname, "static/index.html"), (err, html) => {
-					if (err) return res.statusCode = 500, res.end(err.toString()), console.error(err);
-					cache.html = html.toString();
-					res.setHeader("Content-type", "text/html; charset=utf-8");
-					res.end(cache.html);
-				});
-				break;
-			case "/style.json":
+		server = createServer(async (req, res) => {
 
-				// try from cache
-				if (cache.style) return res.setHeader("Content-type", "application/json; charset=utf-8"), res.end(cache.style);
+			if (req.method !== 'GET') return respondWithError('Method not allowed', 405);
+			const p = url.parse(req.url).pathname;
 
-				// construct style.json
-				let bbox;
+			// construct base url from request headers
+			//const baseurl = this.opt.base || (req.headers["x-forwarded-proto"] || "http") + '://' + (req.headers["x-forwarded-host"] || req.headers.host);
+
+			if ((p === '/') || (p === '/index.html')) {
 				try {
-					bbox = await this.getBoundingBox()
+					const html = readFile(resolve(__dirname, 'static/index.html'));
+					return respondWithContent(html, 'text/html; charset=utf-8');
 				} catch (err) {
-					return res.statusCode = 500, res.end(err.toString()), console.error(err);
+					return respondWithError(err, 500);
+				}
+			}
+
+			if (match = p.match(/^\/tiles\/(?<name>)\/(tile|meta)\.json/)) {
+				let { name } = match.groups;
+				const layer = layers[name];
+				if (!layer) return respondWithError(`layer ${name} not found`, 404);
+				try {
+					return respondWithContent(await layer.container.getMeta(), 'application/json; charset=utf-8', 'br');
+				} catch (err) {
+					return respondWithError(err, 500);
+				}
+			}
+
+			if (match = p.match(/^\/tiles\/(?<name>)\/(?<z>[0-9]+)\/(?<x>[0-9]+)\/(?<y>[0-9]+).*/)) {
+				let { name, x, y, z } = match.groups;
+				const layer = layers[name];
+				if (!layer) return respondWithError(`layer ${name} not found`, 404);
+				try {
+					x = parseInt(x, 10);
+					y = parseInt(y, 10);
+					z = parseInt(z, 10);
+					return respondWithContent(await layer.container.getTile(z, x, y), layer.mime, layer.precompression);
+				} catch (err) {
+					return respondWithError(err, 500);
+				}
+			}
+
+			async function respondWithContent(content, mime, compression) {
+				const accepted_encoding = req.headers['accept-encoding'] || '';
+				const accept_gzip = accepted_encoding.includes('gzip');
+				const accept_br = accepted_encoding.includes('br');
+
+				switch (compression) {
+					case 'br':
+						if (accept_br) break;
+						if (recompress && accept_gzip) {
+							content = await gzip(await unbrotli(content));
+							compression = 'gzip';
+							break;
+						}
+						content = await unbrotli(content);
+						compression = null;
+						break;
+					case 'gzip':
+						if (accept_gzip) break;
+						content = await ungzip(content);
+						compression = null;
+						break;
+					default:
+						if (recompress && accept_br) {
+							content = await brotli(content);
+							compression = 'br';
+							break;
+						}
+						if (recompress && accept_gzip) {
+							content = await gzip(content);
+							compression = 'gzip';
+							break;
+						}
+						compression = null;
+						break;
 				}
 
-				const center = [
-					((bbox[0] + bbox[2]) / 2),
-					((bbox[1] + bbox[3]) / 2)
-				];
+				if (compression) res.setHeader('content-encoding', compression);
 
-				let zoom;
-				try {
-					zoom = await this.getZoomLevels()
-				} catch (err) {
-					return res.statusCode = 500, res.end(err.toString()), console.error(err);
-				}
+				res.statusCode = 200;
+				res.setHeader('content-type', mime);
+				res.end(content);
+			}
 
-				const zooms = [
-					parseInt(zoom[0], 10),
-					parseInt(zoom[zoom.length - 1], 10),
-				];
-				const midzoom = Math.round((zooms[0] + zooms[1]) / 2);
+			function respondWithError(err, code = 500) {
+				console.error(err);
+				res.statusCode = code;
+				res.setHeader('content-type', 'text/plain');
+				res.end(err.toString());
+			}
+		});
 
-				const style = {
-					version: 8,
-					id: "versatiles",
-					name: "versatiles",
-					zoom: midzoom,
-					center: center,
-					sources: {},
-					layers: [],
-				};
+		const port = arguments.port ?? 8080;
+		server.listen(port);
+		console.log(`listening on port ${port} `);
 
-				if (this.header.tile_format === "pbf") { // vector tiles
-					style.sources.versatiles = {
-						type: "vector",
-						url: baseurl + "/tile.json",
-					};
-					// FIXME: extract layers from metadata
-				} else { // raster tiles
-					style.sources.versatiles = {
-						type: "raster",
-						tiles: [baseurl + "/{z}/{x}/{y}"],
-						tileSize: 256,
-					};
-					style.layers.push({
-						id: "versatiles",
-						type: "raster",
-						source: "versatiles",
-						minzoom: zooms[0],
-						maxzoom: zooms[1],
-					});
-				};
-
-				cache.style = JSON.stringify(style, null, "\t");
-				res.setHeader("Content-type", "application/json; charset=utf-8");
-				return res.end(cache.style);
-			case "/tile.json":
-
-				// try from cache
-				if (cache.tilejson) return res.setHeader("Content-type", "application/json; charset=utf-8"), res.end(cache.tilejson);
-
-				// construct tilejson, extend with metadata
-				// https://github.com/mapbox/tilejson-spec/tree/master/3.0.0
-				let meta;
-				try {
-					meta = await this.getMeta()
-				} catch (err) {
-					return res.statusCode = 500, res.end(err.toString()), console.error(err);
-				}
-
-				// construct tilejson
-				meta.tilejson = "3.0.0";
-				meta.tiles = [baseurl + "/{z}/{x}/{y}"];
-				meta.scheme = meta.scheme || "zxy";
-
-				if (!meta.vector_layers) meta.vector_layers = []; // for good luck!
-
-				try {
-					meta.bounds = await this.getBoundingBox();
-				} catch (err) { }
-
-				try {
-					let zoom = await this.getZoomLevels();
-					meta.minzoom = meta.minzoom || parseInt(zoom[0], 10);
-					meta.maxzoom = meta.maxzoom || parseInt(zoom[zoom.length - 1], 10);
-				} catch (err) { }
-
-				cache.tilejson = JSON.stringify(meta, null, "\t");
-				res.setHeader("Content-type", "application/json; charset=utf-8");
-
-				return res.end(cache.tilejson);
-			default: // get tile (TODO: cache tiles)
-
-				const xyz = p.split("/")
-					.filter(c => !!c) // this is good enough
-					.map(c => parseInt(c, 10)) // getTiles() eats integers
-
-				if (xyz.length < 3) return res.statusCode = 404, res.end("sorry");
-
-				try {
-					let tile = await this.getTile(xyz[0], xyz[1], xyz[2]);
-
-					if (tile.length === 0) return res.statusCode = 204, res.end(); // empty tile → "204 no content"
-					res.setHeader("Content-type", this.mimetypes[this.header.tile_format]);
-
-					// not compressed anyway
-					if (this.header.tile_precompression === null) return res.end(tile);
-
-					// can the client eat the precompression?
-					const accepted_encodings = (req.headers["accept-encoding"] || "").split(/, */g).map(e => e.split(";").shift());
-
-					// no, decompression required
-					if (accepted_encodings.includes(encodings[this.header.tile_precompression])) return res.setHeader("Content-Encoding", encodings[this.header.tile_precompression]), res.end(tile);
-
-					// decompress and deliver
-					this.decompress(this.header.tile_precompression, tile, (err, tile) => {
-						if (err) return res.statusCode = 500, res.end(err.toString()), console.error(err);
-						res.end(tile);
-					});
-				} catch (err) {
-					return res.statusCode = 500, res.end(err.toString()), console.error(err);
-				}
-				break;
-		}
-
-	});
-
-	arguments.port ??= 8080;
-	srvr.listen(arguments.port);
-	console.log('listening on port ' + arguments.port);
-
-	return srvr;
+		return server;
+	}
 }
 
 // executable magic
@@ -202,4 +198,36 @@ if (require.main === module) {
 		if (err) return console.error(err.toString()), process.exit(1);
 		console.error("Listening on http://%s:%d/", host, port);
 	});
+}
+
+function gzip(dataIn) {
+	return new Promise((res, rej) =>
+		zlib.gzip(dataIn, { level: 9 }, (err, dataOut) => {
+			if (err) return rej(err); res(dataOut);
+		})
+	)
+}
+
+function ungzip(dataIn) {
+	return new Promise((res, rej) =>
+		zlib.ungzip(dataIn, (err, dataOut) => {
+			if (err) return rej(err); res(dataOut);
+		})
+	)
+}
+
+function brotli(dataIn) {
+	return new Promise((res, rej) =>
+		zlib.brotli(dataIn, { params: { [zlib.constants.BROTLI_PARAM_QUALITY]: 11, } }, (err, dataOut) => {
+			if (err) return rej(err); res(dataOut);
+		})
+	)
+}
+
+function unbrotli(dataIn) {
+	return new Promise((res, rej) =>
+		zlib.unbrotli(dataIn, (err, dataOut) => {
+			if (err) return rej(err); res(dataOut);
+		})
+	)
 }
