7fa5d6bdcf4b07d076838ea157aa93121585b455
Michael Kreil <github@michael-kreil.de>
2023-10-24 17:34:45 +0200

    update interface

--- versatiles/src/interfaces.ts
+++ versatiles/src/interfaces.ts
@@ -1,50 +1,53 @@
-
 /**
- * Different types of supported compressions. `null` means uncompressed.
+ * Supported compression.
+ * `null` signifies that the data is uncompressed.
  */
 export type Compression = 'gzip' | 'br' | null;
 
 
-
 /**
- * Different file formats.
+ * Supported tile formats.
  */
 export type Format = 'avif' | 'bin' | 'geojson' | 'jpeg' | 'json' | 'pbf' | 'png' | 'svg' | 'topojson' | 'webp' | null;
 
 
-
 /**
- * Decompressor function type
+ * Type definition for a function that decompresses data.
+ * 
+ * @param {Buffer} data - The compressed data.
+ * @param {Compression} compression - The type of compression used.
+ * @returns {Promise<Buffer>} - The decompressed data.
  */
 export type Decompressor = (data: Buffer, compression: Compression) => Promise<Buffer>;
 
 
 
 /**
- * Defines an asynchronous container reader function.
- * It's basically a function that returns `length`s bytes starting at `position` of a container file.
- * You can define your own reader function to access containers via any network/interface/hardware.
- * @property {number} position - offset of first byte to read
- * @property {number} length - number of bytes to read
+ * Type definition for an asynchronous function to read a container's content.
+ * 
+ * This can be useful for accessing data stored in various storage mediums.
+ * @param {number} position - The byte offset at which to start reading.
+ * @param {number} length - The number of bytes to read.
+ * @returns {Promise<Buffer>} - The read data.
  */
 export type Reader = (position: number, length: number) => Promise<Buffer>;
 
 
 
-
 /**
- * Defines the header of a container.
- * @property {string} magic - first bytes of the container, usually "versatiles_v02"
- * @property {string} version - version of the versatiles container, usually "v02"
- * @property {Format} tileFormat - file format of the stored tiles
- * @property {Compression} tileCompression - compression of the stored tiles
- * @property {number} zoomMin - minimum zoom level
- * @property {number} zoomMax - maximum zoom level
- * @property {[number, number, number, number]} bbox - bounding box of this container: `[lon_min, lat_min, lon_min, lat_min]`
- * @property {number} metaOffset - position of the first byte of the metadata inside the container. metadata is compressed with `tileCompression`.
- * @property {number} metaLength - length of the metadata in bytes. `metaLength = 0` means there is no metadata.
- * @property {number} blockIndexOffset - position of the first byte of the block index inside the container. block index is compressed with brotli.
- * @property {number} blockIndexLength - length of the block index in bytes. `blockIndexLength = 0` means there aren't any tiles in the container.
+ * Interface for the metadata header of a `*.Versatiles` container.
+ * 
+ * @property {string} magic - Identifier for the container format, usually "versatiles_v02".
+ * @property {string} version - Version of the container format, typically "v02".
+ * @property {Format} tileFormat - The format used for storing tiles.
+ * @property {Compression} tileCompression - The type of compression applied to tiles.
+ * @property {number} zoomMin - The minimum zoom level.
+ * @property {number} zoomMax - The maximum zoom level.
+ * @property {[number, number, number, number]} bbox - Bounding box coordinates as [lon_min, lat_min, lon_max, lat_max].
+ * @property {number} metaOffset - The byte offset for metadata within the container.
+ * @property {number} metaLength - The byte size of the metadata. A value of 0 means no metadata.
+ * @property {number} blockIndexOffset - The byte offset for the block index within the container.
+ * @property {number} blockIndexLength - The byte size of the block index. A value of 0 indicates no tiles in the container.
  */
 export interface Header {
 	magic: string;
@@ -63,19 +66,21 @@ export interface Header {
 
 
 /**
- * Defines a block of tiles, including all necessary metadata. see also the [spec v02](https://github.com/versatiles-org/versatiles-spec/blob/main/v02/readme.md#block_index)
- * @property {number} level - zoom level
- * @property {number} column - column of the block in this zoom level
- * @property {number} row - row of the block in this zoom level
- * @property {number} colMin - minimum column where a tile is stored in this block (0..255)
- * @property {number} rowMin - minimum row where a tile is stored in this block (0..255)
- * @property {number} colMax - maximum column where a tile is stored in this block (0..255)
- * @property {number} rowMax - maximum row where a tile is stored in this block (0..255)
- * @property {number} blockOffset - byte position of this block in the container
- * @property {number} tileIndexOffset - byte position of the tile index in the container
- * @property {number} tileIndexLength - byte length of the tile index
- * @property {number} tileCount - number of tiles in this block
- * @property {TileIndex=} tileIndex - tile index, if it was already fetched
+ * Interface for a block of tiles including necessary metadata.
+ * For more details, refer to [spec v02](https://github.com/versatiles-org/versatiles-spec/blob/main/v02/readme.md#block_index).
+ * 
+ * @property {number} level - The zoom level for this block.
+ * @property {number} column - The column position of this block at the current zoom level.
+ * @property {number} row - The row position of this block at the current zoom level.
+ * @property {number} colMin - Minimum column index for tiles stored in this block (range: 0-255).
+ * @property {number} rowMin - Minimum row index for tiles stored in this block (range: 0-255).
+ * @property {number} colMax - Maximum column index for tiles stored in this block (range: 0-255).
+ * @property {number} rowMax - Maximum row index for tiles stored in this block (range: 0-255).
+ * @property {number} blockOffset - Byte position where this block starts in the file container.
+ * @property {number} tileIndexOffset - Byte position where the tile index starts within the container.
+ * @property {number} tileIndexLength - Length of the tile index in bytes.
+ * @property {number} tileCount - The number of tiles contained in this block.
+ * @property {TileIndex=} tileIndex - Optional tile index if it has been fetched.
  */
 export interface Block {
 	level: number;
@@ -95,9 +100,10 @@ export interface Block {
 
 
 /**
- * Defines an index of tiles inside a block.
- * @property {Float64Array} offsets - array of positions of the first byte of the tiles inside the block. tiles are compressed with `tileCompression`.
- * @property {Float64Array} lengths - array of length of the tiles in bytes. `lengths[i] = 0` means that this tile does not stored.
+ * Interface for the index structure used for tiles within a block.
+ * 
+ * @property {Float64Array} offsets - Array indicating the start byte positions of tiles within the block.
+ * @property {Float64Array} lengths - Array specifying the byte lengths of the tiles. A length of 0 means the tile is not stored.
  */
 export interface TileIndex {
 	offsets: Float64Array;
@@ -107,9 +113,9 @@ export interface TileIndex {
 
 
 /**
- * Defines supported options for reading a container.
- * @property {boolean} tms - if true, the [TMS (Tile Map Service) tile ordering](https://wiki.openstreetmap.org/wiki/TMS)  is used, so y = 0 means south
+ * Interface for defining the options available for reading a container.
+ * @property {boolean} tms - If set to true, uses the [TMS (Tile Map Service) tile ordering](https://wiki.openstreetmap.org/wiki/TMS) where y=0 is the southernmost point.
  */
 export interface Options {
-	tms: boolean
+	tms: boolean;
 }
