ac01f4ff7b5fe8296e1ef2bb1f121f2208624f8e
Michael Kreil <github@michael-kreil.de>
2023-11-08 21:16:09 +0100

    doc: compressors

--- server/src/lib/compressors.ts
+++ server/src/lib/compressors.ts
@@ -1,6 +1,11 @@
 
 import zlib from 'node:zlib';
 
+/**
+ * Compresses data using Gzip with maximum compression level.
+ * @param dataIn - The input data as a Buffer to be compressed.
+ * @returns A Promise that resolves to the Gzip-compressed Buffer.
+ */
 export async function gzip(dataIn: Buffer): Promise<Buffer> {
 	return new Promise((res, rej) => {
 		zlib.gzip(dataIn, { level: 9 }, (err, dataOut) => {
@@ -14,6 +19,11 @@ export async function gzip(dataIn: Buffer): Promise<Buffer> {
 	);
 }
 
+/**
+ * Decompresses Gzip-compressed data.
+ * @param dataIn - The Gzip-compressed data as a Buffer to be decompressed.
+ * @returns A Promise that resolves to the decompressed Buffer.
+ */
 export async function ungzip(dataIn: Buffer): Promise<Buffer> {
 	return new Promise((res, rej) => {
 		zlib.gunzip(dataIn, (err, dataOut) => {
@@ -27,6 +37,11 @@ export async function ungzip(dataIn: Buffer): Promise<Buffer> {
 	);
 }
 
+/**
+ * Compresses data using Brotli with maximum quality parameter.
+ * @param dataIn - The input data as a Buffer to be compressed.
+ * @returns A Promise that resolves to the Brotli-compressed Buffer.
+ */
 export async function brotli(dataIn: Buffer): Promise<Buffer> {
 	return new Promise((res, rej) => {
 		zlib.brotliCompress(dataIn, { params: { [zlib.constants.BROTLI_PARAM_QUALITY]: 11 } }, (err, dataOut) => {
@@ -40,6 +55,11 @@ export async function brotli(dataIn: Buffer): Promise<Buffer> {
 	);
 }
 
+/**
+ * Decompresses Brotli-compressed data.
+ * @param dataIn - The Brotli-compressed data as a Buffer to be decompressed.
+ * @returns A Promise that resolves to the decompressed Buffer.
+ */
 export async function unbrotli(dataIn: Buffer): Promise<Buffer> {
 	return new Promise((res, rej) => {
 		zlib.brotliDecompress(dataIn, (err, dataOut) => {
