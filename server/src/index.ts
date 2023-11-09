3ed3c64487c2351803da8575a5e0a6709047537e
Michael Kreil <github@michael-kreil.de>
2023-11-09 20:58:03 +0100

    fix: processing options

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@
 
 import { Command } from 'commander';
 import { Server } from './lib/server.js';
+import type { Options } from './lib/server.js';
 
 const program = new Command();
 
@@ -10,15 +11,26 @@ program
 	.showHelpAfterError()
 	.name('versatiles-server')
 	.description('Simple VersaTiles server')
-	.option('-t, --tms', 'use TMS tile order (flip y axis)')
-	.option('-c, --compress', 'compress data if needed. Slower, but reduces traffic.')
-	.option('-p, --port <port>', 'port' )
-	.option('-i, --host <hostname|ip>', 'hostname or ip')
-	.option('-b, --base <http://baseurl/>', 'default is: "http://localhost:<port>/"')
+	.option('-b, --base-url <http://baseurl/>', 'default is: "http://localhost:<port>/"')
+	.option('-c, --compress', 'compress data if needed. Slower, but reduces traffic.', true)
+	.option('-i, --host <hostname|ip>', 'hostname or ip', '0.0.0.0')
+	.option('-p, --port <port>', 'port', '8080')
+	.option('-t, --tms', 'use TMS tile order (flip y axis)', false)
 	.argument('<source>', 'VersaTiles container, can be an url or filename of a "*.versatiles" file');
 
 program.parse();
 
-const server = new Server(program.args[0], program.opts());
+const commandOptions = program.opts();
 
-void server.start();
+const serverOptions: Options = {
+	baseUrl: commandOptions.baseUrl as string | undefined,
+	compress: commandOptions.compress as boolean | undefined,
+	host: commandOptions.host as string | undefined,
+	port: commandOptions.port as number | undefined,
+	tms: commandOptions.tms as boolean | undefined,
+};
+
+const server = new Server(program.args[0], serverOptions);
+
+await server.start();
+
