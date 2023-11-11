a9875be68bfb775d96d3546ed4a8d8687d552ed1
Michael Kreil <github@michael-kreil.de>
2023-11-11 15:06:59 +0100

    refactor: index -> server

--- versatiles-server/src/server.ts
+++ versatiles-server/src/server.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { Server } from './lib/server.js';
 import type { ServerOptions } from './lib/types.js';
 import open from 'open';
+import { Server } from './lib/server.js';
 
 /**
  * Entry script for the VersaTiles server command-line application.
@@ -22,30 +22,28 @@ program
 	.option('-o, --open', 'Open map in web browser', false)
 	.option('-p, --port <port>', 'Port to bind the server to', parseInt, 8080)
 	.option('-t, --tms', 'Use TMS tile order (flip y axis)', false)
-	.argument('<source>', 'VersaTiles container, can be a URL or filename of a "*.versatiles" file');
+	.argument('<source>', 'VersaTiles container, can be a URL or filename of a "*.versatiles" file')
+	.action(async (source: string, cmdOptions: Record<string, unknown>) => {
+		const srvOptions: ServerOptions = {
+			baseUrl: cmdOptions.baseUrl as string | undefined,
+			compress: cmdOptions.compress as boolean | undefined,
+			host: cmdOptions.host as string | undefined,
+			port: cmdOptions.port as number | undefined,
+			tms: cmdOptions.tms as boolean | undefined,
+		};
 
+		try {
+			const server = new Server(program.args[0], srvOptions);
+			await server.start();
+
+			if (Boolean(cmdOptions.open)) {
+				await open(server.getUrl());
+			}
+		} catch (error: unknown) {
+			const errorMessage = String((typeof error == 'object' && error != null && 'message' in error) ? error.message : error);
+			console.error(`Error starting the server: ${errorMessage}`);
+			process.exit(1);
+		}
+	});
 
 program.parse();
-
-const cmdOptions = program.opts();
-
-const srvOptions: ServerOptions = {
-	baseUrl: cmdOptions.baseUrl as string | undefined,
-	compress: cmdOptions.compress as boolean | undefined,
-	host: cmdOptions.host as string | undefined,
-	port: cmdOptions.port as number | undefined,
-	tms: cmdOptions.tms as boolean | undefined,
-};
-
-try {
-	const server = new Server(program.args[0], srvOptions);
-	await server.start();
-
-	if (Boolean(cmdOptions.open)) {
-		await open(server.getUrl());
-	}
-} catch (error: unknown) {
-	const errorMessage = String((typeof error == 'object' && error != null && 'message' in error) ? error.message : error);
-	console.error(`Error starting the server: ${errorMessage}`);
-	process.exit(1);
-}
