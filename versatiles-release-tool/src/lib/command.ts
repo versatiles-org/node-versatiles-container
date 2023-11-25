6c6b319b07cf3f82581013656ad3f35f9addaaeb
Michael Kreil <github@michael-kreil.de>
2023-11-25 16:32:57 +0100

    refactor: commant

--- versatiles-release-tool/src/lib/command.ts
+++ versatiles-release-tool/src/lib/command.ts
@@ -1,4 +1,5 @@
 import { spawnSync } from 'node:child_process';
+import { getErrorMessage } from './utils.js';
 
 export function generateCommandDocumentation(command: string): string {
 	const result = getCommandResults(command);
@@ -6,42 +7,47 @@ export function generateCommandDocumentation(command: string): string {
 	const { subcommands } = result;
 
 	for (const subcommand of subcommands) {
-		const subResult = getCommandResults(command + ' ' + subcommand);
-		markdown += `\n# Subcommand: \`${command} ${subcommand}\`\n\n${subResult.markdown}`;
+		const fullCommand = `${command} ${subcommand}`;
+		try {
+			const subResult = getCommandResults(fullCommand);
+			markdown += `\n# Subcommand: \`${fullCommand}\`\n\n${subResult.markdown}`;
+		} catch (error) {
+			throw new Error(`Error generating documentation for subcommand '${fullCommand}': ${getErrorMessage(error)}`);
+		}
 	}
 	return markdown;
 }
 
 function getCommandResults(command: string): { markdown: string; subcommands: string[] } {
-	const cp = spawnSync(
-		'npx',
-		[...command.split(' '), '--help'],
-	);
+	const cp = spawnSync('npx', [...command.split(' '), '--help']);
 
-	if (cp.error) throw Error(cp.error.toString());
+	if (cp.error) throw new Error(cp.error.toString());
+	if (cp.status !== 0) throw new Error(`Command failed with exit code ${cp.status}`);
+
 	const result = cp.stdout.toString().trim();
+	console.log(`Command executed successfully: ${command}`);
 
-	const subcommands = result
+	return {
+		markdown: formatResultAsMarkdown(command, result),
+		subcommands: extractSubcommands(result),
+	};
+}
+
+function extractSubcommands(result: string): string[] {
+	return result
 		.replace(/.*\nCommands:/msgi, '')
 		.replace(/\n[a-z]+:.*/msi, '')
 		.split('\n')
 		.flatMap((line): string[] => {
 			const extract = /^  ([^ ]{2,})/.exec(line);
 			if (!extract) return [];
-			// eslint-disable-next-line @typescript-eslint/prefer-destructuring
-			const subcommand = extract[1];
+
+			const [, subcommand] = extract;
 			if (subcommand === 'help') return [];
 			return [subcommand];
 		});
+}
 
-	return {
-		markdown: [
-			'```console',
-			'$ ' + command,
-			result,
-			'```',
-			'',
-		].join('\n'),
-		subcommands,
-	};
+function formatResultAsMarkdown(command: string, result: string): string {
+	return `\`\`\`console\n$ ${command}\n${result}\`\`\`\n`;
 }
---
+++ versatiles-release-tool/src/lib/utils.ts
@@ -0,0 +1,8 @@
+
+export function getErrorMessage(error: unknown): string {
+	if (error == null) return 'unknown'
+	if (typeof error === 'object') {
+		if ('message' in error) return String(error.message);
+	}
+	return 'unknown'
+}
